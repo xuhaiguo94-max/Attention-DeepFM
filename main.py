5d36346d4fd827149bece8df5a6e38ec8682ea66
xuhaiguo94-max <xuhaiguo94@gmail.com>
2026-06-15 21:10:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from model.DeepFM import DeepFM
+from model.AttentionDeepFM import AttentionDeepFM
 from data.dataset import CriteoDataset
 
-# ============ 1. 显存防爆设置 ============
+# ============ 显存防爆设置 ============
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     try:
@@ -13,36 +13,36 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-# ============ 2. 极速数据加载 ============
-# 对于 100 万条数据，BatchSize 必须放大，2048 是经典配置，能大幅提高 GPU 利用率
+# ============ 数据加载 ============
 batch_size = 2048 
-
 dataset = CriteoDataset('./data', train=True)
+
 Xi, Xv, y = dataset.Xi, dataset.Xv, dataset.target
 
 total_samples = len(y)
+
 num_train = int(total_samples * 0.8)
 
-# 划分训练集和验证集 (直接切片 Numpy 数组，内存零损耗)
+# 划分训练集和验证集 
 Xi_train, Xv_train, y_train = Xi[:num_train], Xv[:num_train], y[:num_train]
 Xi_val, Xv_val, y_val = Xi[num_train:], Xv[num_train:], y[num_train:]
 
-# 构建全速 TensorFlow 数据管道 (加入 prefetch 提前预加载)
+# 构建 TensorFlow 数据管道 
 loader_train = tf.data.Dataset.from_tensor_slices((Xi_train, Xv_train, y_train)).shuffle(10000).batch(batch_size).prefetch(tf.data.AUTOTUNE)
 loader_val = tf.data.Dataset.from_tensor_slices((Xi_val, Xv_val, y_val)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
-# ============ 3. 计算特征维度 ============
+# ============ 计算特征维度 ============
 
 feature_sizes = [int(np.max(Xi[:, i, 0])) + 1 for i in range(Xi.shape[1])]
 
-# ============ 4. 实例化与训练 ============
-model = DeepFM(feature_sizes)
+# ============ 实例化与训练 ============
+model = AttentionDeepFM(feature_sizes)
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
 
 print("开始正式训练")
 history = model.fit_model(loader_train, loader_val, optimizer, epochs=20)
 
-# ============ 5. 结果可视化 ============
+# ============  结果可视化 ============
 #print("正在绘制并保存 Loss 和 AUC 曲线...")
 plt.figure(figsize=(12, 5))
 
@@ -67,6 +67,6 @@ plt.legend()
 plt.grid(True)
 
 plt.tight_layout()
-#plt.savefig('DeepFM_training_curves.png') # 图片会保存在你代码运行的根目录下
-#print("训练完成！曲线图已保存为 DeepFM_training_curves.png，同时显示如下：")
+#plt.savefig('AttentionDeepFM_training_curves.png') # 图片会保存在你代码运行的根目录下
+#print("训练完成！曲线图已保存为 AttentionDeepFM_training_curves.png，同时显示如下：")
 plt.show()
